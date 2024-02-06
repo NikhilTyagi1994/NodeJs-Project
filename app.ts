7d759c1db8ddbb2bd5e29b3659db30d175b8eed9
Nikhil Tyagi <nikhiltyagi005@gmail.com>
2024-02-06 14:33:05 +0530

    operation function created

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
-
+import airthmeticRoute from "./routes/arithmeticRoutes";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const port:number = 3000;
+
+app.use(express.json());
+
+app.use('/arithmetic', airthmeticRoute)
 
 
 app.get("/test", (req: Request, res: Response) => {
---
+++ routes/arithmeticRoutes.ts
@@ -0,0 +1,29 @@
+import { Express, Response, Router } from "express";
+
+const router: any = Router();
+
+router.post("/operation", (req: any, res: any) => {
+  const { num1, num2, operation } = req.body;
+
+  let result: number = 0;
+  switch (operation) {
+    case "addition":
+      result = num1 + num2;
+      break;
+    case "substraction":
+      result = num1 - num2;
+      break;
+    case "multiplication":
+      result = num1 * num2;
+      break;
+    case "division":
+      result = num1 / num2;
+      break;
+    default:
+      return res.status(201).json({ error: "Invalid operation" });
+  }
+
+  res.status(201).json({ message: `${operation} done`, result: result });
+});
+
+export default router;
